91a13323e31a5cb630ed9b536a3b7fa5eeaff4a5
alexbrunoliveira <alexbrunoliveira@gmail.com>
2022-02-12 19:47:08 -0300

    Criando projeto

---
+++ src/cursojava/classesauxiliares/FuncaoAutentincacao.java
@@ -0,0 +1,18 @@
+package cursojava.classesauxiliares;
+
+import cursojava.interfaces.PermitirAcesso;
+
+/*Receber somente quem tem o contrato da interface de permitir o acesso e chamar o autenticar*/
+
+public class FuncaoAutentincacao {
+	
+	private PermitirAcesso permitirAcesso;
+	
+	public boolean autenticar() {
+		return permitirAcesso.autenticar();
+	}
+	
+	public FuncaoAutentincacao(PermitirAcesso acesso) {
+		this.permitirAcesso = acesso;
+	}
+}
